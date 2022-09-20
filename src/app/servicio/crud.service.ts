3e599dab40f29f0d06830788f5fbcd5a3dea3d14
jeansjh <jhurtadohuanca@gmail.com>
2022-09-20 10:14:05 -0500

    modificacion de las api por id de enfermedades

--- src/app/servicio/crud.service.ts
+++ src/app/servicio/crud.service.ts
@@ -9,16 +9,16 @@ API: string="http://localhost/apiGeriavida/galeria.php";
 
 API2: string="http://localhost/apigeriavida/enfermedades.php/?consultar=1";
 
-API3: string="http://localhost/apigeriavida/enfermedades.php/?consultar=10";
-API4: string="http://localhost/apigeriavida/enfermedades.php/?consultar=11";
-API5: string="http://localhost/apigeriavida/enfermedades.php/?consultar=12";
-API6: string="http://localhost/apigeriavida/enfermedades.php/?consultar=13";
-API7: string="http://localhost/apigeriavida/enfermedades.php/?consultar=14";
-API8: string="http://localhost/apigeriavida/enfermedades.php/?consultar=15";
-API9: string="http://localhost/apigeriavida/enfermedades.php/?consultar=16";
-API10: string="http://localhost/apigeriavida/enfermedades.php/?consultar=17";
-API11: string="http://localhost/apigeriavida/enfermedades.php/?consultar=18";
-API12: string="http://localhost/apigeriavida/enfermedades.php/?consultar=19";
+API3: string="http://localhost/apigeriavida/enfermedades.php/?consultar=2";
+API4: string="http://localhost/apigeriavida/enfermedades.php/?consultar=3";
+API5: string="http://localhost/apigeriavida/enfermedades.php/?consultar=4";
+API6: string="http://localhost/apigeriavida/enfermedades.php/?consultar=5";
+API7: string="http://localhost/apigeriavida/enfermedades.php/?consultar=6";
+API8: string="http://localhost/apigeriavida/enfermedades.php/?consultar=7";
+API9: string="http://localhost/apigeriavida/enfermedades.php/?consultar=8";
+API10: string="http://localhost/apigeriavida/enfermedades.php/?consultar=9";
+API11: string="http://localhost/apigeriavida/enfermedades.php/?consultar=10";
+API12: string="http://localhost/apigeriavida/enfermedades.php/?consultar=11";
 
 
 constructor(private clienteHttp:HttpClient) { }
